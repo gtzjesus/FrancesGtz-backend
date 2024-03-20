688b96f4c924dcc0e09b22bc2fca72eea5c2f636
gtz.jesus <gtz.jesus@outlook.com>
2024-03-20 11:33:07 -0600

    Backend modded needed for connection with frontend

--- src/server.mjs
+++ src/server.mjs
@@ -4,39 +4,29 @@ import cors from 'cors';
 import supabaseRoute from './routes/supabaseRoute.mjs'; // Import your Supabase routes
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Update CORS configuration to allow requests from your frontend URL
 app.use(
   cors({
     origin: '*',
-    methods: ['GET', 'POST'],
+    methods: ['GET'], // Allow only GET requests
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
-
-// Handle preflight requests
-app.options('/api/sendEmail', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  res.status(200).end();
-});
 
 // Use Supabase routes
 app.use('/', supabaseRoute);
 
 // Error Handling Middleware
-// app.use((err, req, res, next) => {
-//   res.status(500).send('Something broke!');
-// });
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('WorldHello!');
+app.use((err, req, res, next) => {
+  res.status(500).send('Something broke!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
 });
