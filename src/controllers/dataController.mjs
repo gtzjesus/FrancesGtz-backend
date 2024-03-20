e31ef5d56af4a2c8e79a08d558f36445e81adbcf
gtz.jesus <gtz.jesus@outlook.com>
2024-03-20 11:49:05 -0600

    issues resolved

--- src/controllers/dataController.mjs
+++ src/controllers/dataController.mjs
@@ -1,7 +1,7 @@
 import supabase from '../services/supabase.mjs';
 
 export async function getHouses() {
-  // Query all columns from the 'houses' database
+  // Code logic to query all columns from the 'houses' table
   const { data: houses, error } = await supabase.from('houses').select('*');
   if (error) {
     throw new Error('Houses could not be fetched');
--- src/routes/supabaseRoute.mjs
+++ src/routes/supabaseRoute.mjs
@@ -1,22 +1,21 @@
 import express from 'express';
-import { getHouses } from '../controllers/dataController.mjs';
+import { getHouses } from '../controllers/dataController.mjs'; // Import your Supabase controller function
 
-// Start with creating router to define routes for our API
+// Create a router to define routes for API
 const router = express.Router();
 
-// Define our GET route to aquire houses
-// Callback function gets executed
+// Define a GET route to aquire the houses (callback function gets excecuted)
 router.get('/api/houses', async (req, res) => {
   try {
-    // Call our controller module to fetch our houses
+    // Call controller module to fetch houses data
     const houses = await getHouses();
     res.json(houses);
   } catch (error) {
     console.error('Error fetching houses:', error);
-  }
 
-  // Send error back to client
-  res.status(500).json({ error: 'Failed to fetch houses from Supabase' });
+    // Send error back to client alogn with a JSON object containing the error message
+    res.status(500).json({ error: 'Failed to fetch houses from Supabase' });
+  }
 });
 
 export default router;
